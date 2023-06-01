5cedce53333f918bd9d92ecaf1dfdcb99c61e5d3
tt-hasegawa <tt-hasegawa@matsusaka.co.jp>
2023-06-01 19:34:49 +0900

    add sample.

---
+++ 社内勉強会/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+	// 入力を受け取る
+        String x = sc.next();
+	// メインの処理を書く
+	// 結果を出力する
+        System.out.println(x);
+    }
+}
