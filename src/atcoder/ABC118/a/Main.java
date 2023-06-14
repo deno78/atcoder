e9a27f512ac31412f4e7729aa03688b034948030
tt-hasegawa <tt-hasegawa@matsusaka.co.jp>
2023-06-15 08:35:09 +0900

    朝活

--- src/atcoder/ABC118/a/Main.java
+++ src/atcoder/ABC118/a/Main.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int a=Integer.valueOf(sc.next());
-        int b=Integer.valueOf(sc.next());
+        int a=sc.nextInt();
+        int b=sc.nextInt();
         System.out.println(b%a==0?a+b:b-a);
     }
 }
