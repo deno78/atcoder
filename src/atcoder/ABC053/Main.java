569341df94982e0ca25c4ef68385748d77192792
tt-hasegawa <tt-hasegawa@matsusaka.co.jp>
2023-09-19 08:35:46 +0900

    朝活

---
+++ src/atcoder/ABC053/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println(
+            new Scanner(System.in).nextInt()>=1200?
+            "ARC":
+            "ABC");
+    }
+}
