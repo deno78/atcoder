80f4a1d77a034aa9d38c8d75c596dcb706b87d8d
tetsu hasegawa <tetsu-hasegawa@matsusaka.co.jp>
2023-10-17 13:28:00 +0900

    昼活

---
+++ src/atcoder/ABC060/abc060_a.py
@@ -0,0 +1,10 @@
+# TODO edit this code, this code is for https://atcoder.jp/contests/practice/tasks/practice_1
+
+# param
+a,b,c = input().split()
+
+# solve
+if a[-1]==b[0] and b[-1]==c[0]:
+    print("YES")
+else:
+    print("NO") 
