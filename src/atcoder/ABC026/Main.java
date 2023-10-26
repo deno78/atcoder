cae8711325051dd676d5ce668fe1ecafd895ff8b
tt-hasegawa <tt-hasegawa@matsusaka.co.jp>
2023-10-27 08:46:36 +0900

    朝活

---
+++ src/atcoder/ABC026/Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        int a=new Scanner(System.in).nextInt();
+        System.out.println((a/2)*(a/2));
+    }
+}
