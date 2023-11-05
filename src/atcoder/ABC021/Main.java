8d5d05dd78563ed1941cb8b3b6a4b16fcd9e431c
tt-hasegawa <tt-hasegawa@matsusaka.co.jp>
2023-11-06 08:59:34 +0900

    朝活２

--- src/atcoder/ABC021/Main.java
+++ src/atcoder/ABC021/Main.java
@@ -1,17 +1,23 @@
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        int n = new Scanner(System.in).nextInt();
-        if (n % 2 == 1) {
-            System.out.println(1 + (n - 1) / 2);
-            System.out.println(1);
-            n -= 1;
-        } else {
-            System.out.println(n / 2);
+public class Main{
+  public static void main(String[] args){
+    int[] a=new int[]{8,4,2,1};
+    List ans=new ArrayList();
+    int n = new Scanner(System.in).nextInt();
+    for(int i=0;i<a.length;i++){
+      while(true){
+        if(n>=a[i]){
+          ans.add(a[i]);
+          n-=a[i];
+        }else{
+          break;
         }
-        for (int i = 0; i < n / 2; i++) {
-            System.out.println(2);
-        }
+      }
     }
+    System.out.println(ans.size());
+    for(int i=0;i<ans.size();i++){
+      System.out.println(ans.get(i).toString());
+    }
+  }
 }
