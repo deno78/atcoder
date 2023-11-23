6ffaf391110673d001e413dd981cd54336f3d948
tt-hasegawa <tt-hasegawa@matsusaka.co.jp>
2023-11-24 08:39:24 +0900

    朝活

---
+++ src/atcoder/ABC009/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args){
+        int n = new java.util.Scanner(System.in).nextInt();
+        System.out.println((int) Math.floor(n/2)+n%2);
+    }
+}
