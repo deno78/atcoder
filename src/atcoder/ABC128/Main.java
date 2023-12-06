75fcc7ef5648cfe13f46f2a5447db5de4b67e356
tt-hasegawa <tt-hasegawa@matsusaka.co.jp>
2023-12-07 08:47:31 +0900

    朝活

---
+++ src/atcoder/ABC128/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int p = sc.nextInt();
+        System.out.println((int) Math.floor((a*3+p)/2));
+    }
+}
