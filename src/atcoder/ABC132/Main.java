9e8e8230371c4bdcf25f6e32bf09e0aee174d9b7
tt-hasegawa <tt-hasegawa@matsusaka.co.jp>
2023-12-13 08:48:58 +0900

    朝活２

--- src/atcoder/ABC132/Main.java
+++ src/atcoder/ABC132/Main.java
@@ -2,24 +2,11 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args){
-        String s = new Scanner(System.in).next();
-        Map<Character,Integer> map = new HashMap<Character,Integer>();
-        for(Character c : s.toCharArray()){
-            if(!map.containsKey(c)){
-                map.put(c,0);
-            }
-            map.put(c,map.get(c)+1);
-        }
-        if(map.size()!=2){
-            System.out.println("No");
-            System.exit(0);
-        }
-        for(Map.Entry<Character,Integer> ent:map.entrySet()){
-            if(ent.getValue()!=2){
-                System.out.println("No");
-                System.exit(0);
-            }
-        }
-        System.out.println("Yes");
+        char[] c = new Scanner(System.in).next().toCharArray();
+        Arrays.sort(c);
+        System.out.println(
+            (c[0]==c[1]&&c[2]==c[3]&&c[0]!=c[2])?
+            "Yes":"No"
+        );
     }
 }
