3ea11eb1df036788827bb119b2e49ae905ca4103
tt-hasegawa <tt-hasegawa@matsusaka.co.jp>
2024-01-16 08:56:51 +0900

    朝活

---
+++ src/atcoder/ABC151/a/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        char c = new Scanner(System.in).next().charAt(0);
+        System.out.println(
+            Character.toString((char) c+1)
+        );
+    }
+}
