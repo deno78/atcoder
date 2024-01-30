80b1b2b898487be5f436f48ed5a81c20116cbcc9
tt-hasegawa <tt-hasegawa@matsusaka.co.jp>
2024-01-31 08:41:07 +0900

    朝活

---
+++ src/atcoder/ABC162/a/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        String s = new Scanner(System.in).next();
+        System.out.println(
+            s.contains("7")?"Yes":"No"
+        );
+    }
+}
