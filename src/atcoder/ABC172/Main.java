3b82ff132a2999e09e3025058f35090486d3e82c
tt-hasegawa <tt-hasegawa@matsusaka.co.jp>
2024-02-15 08:40:09 +0900

    朝活

---
+++ src/atcoder/ABC172/Main.java
@@ -0,0 +1,8 @@
+class Main{
+    public static void main(String[] args){
+        int a = new java.util.Scanner(System.in).nextInt();
+        System.out.println(
+            a+a*a+a*a*a
+        );        
+    }
+}
