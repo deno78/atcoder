537a046d289f4aa094f4563ace2d0d73c4b17938
tt-hasegawa <tt-hasegawa@matsusaka.co.jp>
2024-02-28 08:37:24 +0900

    朝活

---
+++ src/atcoder/ABC180/a/Main.java
@@ -0,0 +1,11 @@
+
+class Main {
+    public static void main(String[] args){
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int n = sc.nextInt();       
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(n-a+b);
+    }
+    
+}
