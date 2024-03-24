e4d780d0f1a539d60b2378c4f8a2225426187188
tt-hasegawa <tt-hasegawa@matsusaka.co.jp>
2024-03-25 08:37:48 +0900

    朝活

---
+++ src/atcoder/CodeFestival2016/a/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        String s = new Scanner(System.in).next();
+        System.out.println(s.substring(0,4) + " " + s.substring(4));
+    }
+    
+}
